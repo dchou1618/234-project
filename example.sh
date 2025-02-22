d7cb6c89e9bccc0e23cde81aba343dfec0e93396
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2025-02-22 14:29:59 -0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=/lfs/ampere1/0/dvchou/test-rubiks/DeepCubeA/environments
 export PYTHONPATH=$PYTHONPATH:/lfs/ampere1/0/dvchou/test-rubiks/DeepCubeA/tests
 export PYTHONPATH=$PYTHONPATH:/lfs/ampere1/0/dvchou/test-rubiks/DeepCubeA/
+export CUDA_VISIBLE_DEVICES=0,1,2
