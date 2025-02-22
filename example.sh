146886319cf375f40d3b6b65980ca15f417af75e
dchou1618 <dvchou@andrew.cmu.edu>
2025-02-22 13:34:15 -0800

    example.sh added
    example.sh used for environment setup so tests/timing_test.py may be run.

---
+++ example.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/lfs/ampere1/0/dvchou/test-rubiks/DeepCubeA/environments
+export PYTHONPATH=$PYTHONPATH:/lfs/ampere1/0/dvchou/test-rubiks/DeepCubeA/tests
+export PYTHONPATH=$PYTHONPATH:/lfs/ampere1/0/dvchou/test-rubiks/DeepCubeA/
