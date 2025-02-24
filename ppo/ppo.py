051b5552c96c94ebb1801fa833a3753df18e36be
Benita Wong <benitawong@DN0a1e8d69.SUNet>
2025-02-23 17:09:45 -0800

    ppo modified from https://github.com/WillPalaia/RubiksCubeSolver, with environment modifications upon https://github.com/DoubleGremlin181/RubiksCubeGym, using stable baseline 3

---
+++ ppo/ppo.py
@@ -0,0 +1,65 @@
+import math
+from time import sleep
+import numpy as np
+from stable_baselines3 import PPO
+import torch.nn as nn
+import datetime
+
+from rubiks_cube_222_lbl_ppo_convert_multibinary import RubiksCube222EnvLBLPPOB
+
+date_time = datetime.datetime.now()
+date = date_time.strftime('%m%d%y')
+
+# modified from https://github.com/WillPalaia/RubiksCubeSolver
+# changes made to environment configuration -> integrate with the https://github.com/DoubleGremlin181/RubiksCubeGym environment
+# by converting discrete spaces into multibinary
+# If this is not done, stable baseline 3 runs EXTREMELY slow
+
+def train_rubiks_cube_solver():
+    env = RubiksCube222EnvLBLPPOB()
+    obs, _ = env.reset()
+    policy_kwargs = dict(activation_fn=nn.ReLU,
+                    net_arch=dict(pi=[128, 64, 64], vf=[128, 64, 64]))
+    model = PPO("MlpPolicy", env, verbose=1, policy_kwargs=policy_kwargs, batch_size = 128, learning_rate = 0.00005)
+    print(model.policy)
+
+    training = True
+    if training:
+        for scrambles in range(1, 15):
+            env.scrambles = scrambles
+            env.max_moves = scrambles * 2
+            print(f"training with {scrambles} scrambles, move limit: {env.max_moves}")
+            env.reset()
+            model.learn(total_timesteps=int(50000*env.max_moves*2))
+        model.save(f"ppo-{date}")
+
+    testing = True
+    if testing:
+        env.time_limit = 10
+        for i in range(1,15):
+            stats = []
+            print("env_scramble: " + str(i))
+            moves_i = []
+            for j in range(100):
+                env.scrambles = i
+                env.max_moves = 100
+                obs, _ = env.reset()
+                
+                moves = 0
+                done = False
+                while not done:
+                    action_index, _ = model.predict(obs)
+                    obs, rewards, done, _, info = env.step(action_index.tolist())
+                    moves += 1
+
+                if done and env.check_solved():
+                    moves_i.append(moves)
+                    stats.append("1")
+                else:
+                    stats.append("0")
+
+            print(f"Solves: {stats.count('1')}/{len(stats)}")
+            print(sum(moves_i)/len(moves_i))
+
+if __name__ == "__main__":
+    train_rubiks_cube_solver()
---
+++ ppo/rubiks_cube_222_lbl_ppo_convert_multibinary.py
@@ -0,0 +1,87 @@
+from rubiks_cube_222_multibinary import RubiksCube222EnvB
+import numpy as np
+
+# modified from: https://github.com/DoubleGremlin181/RubiksCubeGym
+# wrapper to convert state space from discrete to multibinary
+
+class RubiksCube222EnvLBLPPOB(RubiksCube222EnvB):
+    def __init__(self):
+        super(RubiksCube222EnvLBLPPOB, self).__init__()
+        self.FL = None
+        self.OLL = None
+        self.time_limit = None
+        self.scrambles = 1
+        self.max_moves = 50
+        self.current_moves = 0
+
+    def check_solved(self):
+        if self.cube_reduced == "WWWWOOGGRRBBOOGGRRBBYYYY":
+            return True
+
+    def reward(self):
+        done = False
+
+        if self.check_solved():
+            reward = 60
+            done = True
+            return reward, done
+
+        reward = -1
+        
+        self.current_moves += 1
+        if self.current_moves >= self.max_moves:
+            reward = -10
+            done = True
+        return reward, done
+
+
+    def step(self, action):
+        obs, reward, done, _, info = super().step(action=action)
+        observation = np.eye(6)[self.convert(obs)].flatten()
+
+        return observation, reward, done, False, info
+
+    def convert(self, obs): # convert discrete state space to multibinary
+        new_obs = [COLOR_IDX[TILE_MAP[obs[i]]] for i in obs]
+        return new_obs
+    
+    def reset(self, *, seed=None, options=None):
+        obs, info = super().reset_scramble(seed=seed, num_scramble=self.scrambles)
+        self.current_moves = 0
+        observation = np.eye(6)[self.convert(obs)].flatten()
+        return observation, info
+    
+    def reset_scramble(self, num_scramble, *, seed=None, options=None):
+        obs, info = super().reset_scramble(seed=seed, num_scramble=num_scramble)
+        observation = np.eye(6)[self.convert(obs)].flatten()
+        return observation, info
+    
+
+
+TILE_MAP = {
+    0: 'W', 1: 'W', 2: 'W', 3: 'W',
+    4: 'O', 5: 'O', 6: 'G', 7: 'G', 8: 'R', 9: 'R', 10: 'B', 11: 'B',
+    12: 'O', 13: 'O', 14: 'G', 15: 'G', 16: 'R', 17: 'R', 18: 'B', 19: 'B',
+    20: 'Y', 21: 'Y', 22: 'Y', 23: 'Y'
+
+}
+
+COLOR_MAP = {
+    'W': (255, 255, 255),
+    'O': (255, 165, 0),
+    'G': (0, 128, 0),
+    'R': (255, 0, 0),
+    'B': (0, 0, 255),
+    'Y': (255, 255, 0)
+}
+
+COLOR_IDX = {
+    'W': 0,
+    'R': 1,
+    'Y': 2,
+    'O': 3, 
+    'B': 4,
+    'G': 5,
+}
+
+ACTION_MAP = {0: ("F", None), 1: ("R", None), 2: ("U", None)}
